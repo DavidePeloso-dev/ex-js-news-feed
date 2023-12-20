0ef122511e24a8c39d461f52a3e8556cc585f780
DavidePeloso-dev <davide.peloso.e@gmail.com>
2023-12-20 22:26:27 +0100

    Add start files

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+const articles = [
+    {
+        title: 'Scoperta di una nuova specie di papera di gomma',
+        content: 'Scoperta di una nuova specie di papera di gomma.',
+        tags: ['geo', 'tech'],
+        author: 'Diana Rossi',
+        published: new Date('2023-02-11'),
+        image: 'rubber-duck.jpg',
+        alt: 'big yellow rubber duck'
+        
+    },
+    {
+        title: 'Esplorando le profondità marine: il mistero degli abissi',
+        content: 'Esplorando le profondità marine: il mistero degli abissi',
+        tags: ['viaggi', 'geo'],
+        author: 'Fabio Mari',
+        published: new Date('2023-03-14'),
+        image: 'deep-sea.jpg',
+        alt: 'deep sea'        
+    },
+    {
+        title: 'Viaggio culinario: alla ricerca dei sapori perduti',
+        content: 'Esplorazione di tradizioni culinarie dimenticate e la ricerca di sapori autentici.',
+        tags: ['cucina'],
+        author: 'Marta Bianchi',
+        published: new Date('2023-04-20'),
+        image: 'kitchen-food.jpg',
+        alt: 'food'        
+    },
+    {
+        title: 'Arte moderna: oltre i confini convenzionali',
+        content: "Un'analisi delle tendenze e delle sfide nell'arte contemporanea, con interviste a artisti emergenti.",
+        tags: ['arte', 'tech'],
+        author: 'Gabriele Neri',
+        published: new Date('2023-05-29'),
+        image: 'modern-art.jpg',
+        alt: 'street art'
+    },
+]
