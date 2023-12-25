ff58950e8c4688e2e49a21a8dbeb401ad50cf54b
DavidePeloso-dev <davide.peloso.e@gmail.com>
2023-12-25 20:12:46 +0100

    Add filter for tags (to verify)

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,46 +43,86 @@ const articles = [
 
 const articleRowEl= document.getElementById('articleRow');
 
+printArticles(articles)
 
+const articlesSaved =[]
 
-articles.forEach(article => {
+const bookmarks = document.querySelectorAll('.fa-bookmark');
 
-    const day = article.published.getDate();
-    const month = (article.published.getMonth()+1);
-    const year = article.published.getFullYear();
-
-    const articleMarckUp = 
-    `<div id="${article.id}" class="col mb-5">
-        <div class="card rounded-0">
-            <div class="card-body">
-                <div class="d-flex justify-content-between gap-3">
-                    <h3 class="card-title">${article.title}</h3>
-                    <i class="fa-solid fa-bookmark fa-xl pt-3"></i>
-                </div>
-                <h6 class="card-subtitle">Pubblicato da ${article.author}</h6>
-                <p class="card-text text-body-secondary">In data ${day+ '/' + (month < 10 ? '0' + month : month) + '/' + year}</p>
-                <p class="card-text">${article.content}</p>
-            </div>
-            <img src="./images/${article.image}" class="px-3" alt="${article.alt}">
-            <div id="tags" class="p-3">
-            ${article.tags.map(tag =>
-                `<span class="badge ${tag} p-2 me-2">${tag}</span>`
-            ).join('')}
-            </div>
-        </div>
-    </div>`;
-
-    console.log(articleMarckUp);
-
-    articleRowEl.insertAdjacentHTML('beforeend', articleMarckUp);
-})
+function clickToSaveArticle(){
+bookmarks.forEach((bookmark, i) => 
+bookmark.addEventListener('click', function(){
+    this.classList.remove('fa-regular');
+    this.classList.add('fa-solid');
+    if(!articlesSaved.includes(articles[i])){
+    articlesSaved.push(articles[i])
+    }
+    console.log(articlesSaved);
+}))}
+clickToSaveArticle();
 
 const geo = articles.filter(article => article.tags.includes('geo'));
-
 const travel = articles.filter(article => article.tags.includes('viaggi'));
-
 const kitchen = articles.filter(article => article.tags.includes('cucina'));
-
 const tech = articles.filter(article => article.tags.includes('tech'));
-
 const art = articles.filter(article => article.tags.includes('arte'));
+
+
+
+const selectTags = document.getElementById('select-tags');
+selectTags.addEventListener('change', function(){
+    if(selectTags.value == '3'){
+        printArticles(art);
+    } else if(selectTags.value == '4'){
+        printArticles(travel);
+    } else if(selectTags.value == '5'){
+        printArticles(tech);
+    } else if (selectTags.value == '6'){
+        printArticles(geo);
+    } else if(selectTags.value == '7'){
+        printArticles(kitchen);
+    } else if(selectTags.value == '2'){
+        articleRowEl.innerHTML = '<h2 class="text-white">No news available.</h2>';
+    } else {
+        printArticles(articles);
+    };
+
+});
+
+/**
+ * ## Stampa oggetti sul DOM
+ * @param {Array} articles array di oggetti da stampare sul DOM
+ */
+function printArticles(articles){
+    articleRowEl.innerHTML = '';
+    articles.forEach(article => {
+    
+        const day = article.published.getDate();
+        const month = (article.published.getMonth()+1);
+        const year = article.published.getFullYear();
+    
+        const articleMarkUp = 
+        `<div id="${article.id}" class="col mb-5">
+            <div class="card rounded-0">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between gap-3">
+                        <h3 class="card-title">${article.title}</h3>
+                        <i class="fa-regular fa-bookmark fa-xl pt-3"></i>
+                    </div>
+                    <h6 class="card-subtitle">Pubblicato da ${article.author}</h6>
+                    <p class="card-text text-body-secondary">In data ${day+ '/' + (month < 10 ? '0' + month : month) + '/' + year}</p>
+                    <p class="card-text">${article.content}</p>
+                </div>
+                <img src="./images/${article.image}" class="px-3" alt="${article.alt}">
+                <div id="tags" class="p-3">
+                ${article.tags.map(tag =>
+                    `<span class="badge ${tag} p-2 me-2">${tag}</span>`
+                ).join('')}
+                </div>
+            </div>
+        </div>`;
+    
+        console.log(articleMarkUp);
+    
+        articleRowEl.insertAdjacentHTML('beforeend', articleMarkUp);
+    })}
